d8386c2a130d026c98f7cb614353831d58fd8d5b
Jordy McNab <jordy@usecapsule.com>
2024-10-21 11:19:07 -0500

    feat: add with react vite starter template for modal

---
+++ with-react-vite/src/App.tsx
@@ -0,0 +1,32 @@
+import { useState } from "react";
+import { capsuleClient } from "./client/capsule";
+import { CapsuleModal, OAuthMethod } from "@usecapsule/react-sdk";
+import "@usecapsule/react-sdk/styles.css";
+import Logo from "./assets/capsule-logo.svg";
+
+export default function App() {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>Capsule Modal Starter Template with React and Vite</h1>
+      <button
+        onClick={() => setIsModalOpen(true)}
+        style={{ padding: "10px 20px", fontSize: "16px", cursor: "pointer" }}>
+        Open Capsule Modal
+      </button>
+      <CapsuleModal
+        capsule={capsuleClient}
+        appName="Capsule Modal Starter Template"
+        logo={Logo}
+        disableEmailLogin={false}
+        disablePhoneLogin={false}
+        oAuthMethods={Object.values(OAuthMethod)}
+        isOpen={isModalOpen}
+        onClose={() => setIsModalOpen(false)}
+        onRampTestMode={true}
+        twoFactorAuthEnabled={false}
+      />
+    </div>
+  );
+}
