bab50fa9a096630ad317a29f0d749070f91b525a
Jordy McNab <jordy@usecapsule.com>
2024-11-03 20:08:46 -0800

    chore: update config

--- with-vue-nuxt/nuxt.config.ts
+++ with-vue-nuxt/nuxt.config.ts
@@ -1,28 +1,11 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
 import react from "@vitejs/plugin-react";
 
+// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: "2024-04-03",
   devtools: { enabled: true },
   ssr: false,
   vite: {
-    plugins: [
-      react({
-        include: [/\.tsx$/, /\.jsx$/],
-        babel: {
-          parserOpts: {
-            plugins: ["jsx"],
-          },
-        },
-      }),
-    ],
-    optimizeDeps: {
-      include: ["react", "react-dom", "@usecapsule/react-sdk"],
-    },
-    esbuild: {
-      jsxInject: `import React from 'react'`, // Automatically inject React
-      jsxFactory: "React.createElement",
-      jsxFragment: "React.Fragment",
-    },
+    plugins: [react()],
   },
 });
