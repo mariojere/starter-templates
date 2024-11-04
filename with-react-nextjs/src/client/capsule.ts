9813c4fd54d3ce7102569610f999f298369a72b6
Jordy McNab <jordy@usecapsule.com>
2024-11-03 17:30:24 -0800

    chore: rename nextjs and bump dependency versions

--- with-react-nextjs/src/client/capsule.ts
+++ with-react-nextjs/src/client/capsule.ts
@@ -2,4 +2,8 @@ import { Environment, CapsuleWeb } from "@usecapsule/react-sdk";
 
 const CAPSULE_API_KEY = process.env.NEXT_PUBLIC_CAPSULE_API_KEY;
 
+if (!CAPSULE_API_KEY) {
+  throw new Error("CAPSULE_API_KEY is not set");
+}
+
 export const capsuleClient = new CapsuleWeb(Environment.BETA, CAPSULE_API_KEY);
