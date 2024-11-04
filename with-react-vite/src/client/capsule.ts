83586dc51531ae25091508519cdc29747f0b883b
Jordy McNab <jordy@usecapsule.com>
2024-11-03 18:43:43 -0800

    chore: fix env import

--- with-react-vite/src/client/capsule.ts
+++ with-react-vite/src/client/capsule.ts
@@ -1,5 +1,5 @@
 import { Environment, CapsuleWeb } from "@usecapsule/react-sdk";
 
-const CAPSULE_API_KEY = process.env.NEXT_PUBLIC_CAPSULE_API_KEY;
+const CAPSULE_API_KEY = import.meta.env.VITE_CAPSULE_API_KEY;
 
 export const capsuleClient = new CapsuleWeb(Environment.BETA, CAPSULE_API_KEY);
