15b79061b7b28f43b97b2c927850aa2483f3e55f
Jordy McNab <jordy@usecapsule.com>
2024-11-03 17:55:02 -0800

    chore: cleanup sorting and dependey versions

--- with-react-vite/src/App.tsx
+++ with-react-vite/src/App.tsx
@@ -1,15 +1,15 @@
 import { useState } from "react";
-import { capsuleClient } from "./client/capsule";
 import { CapsuleModal, OAuthMethod } from "@usecapsule/react-sdk";
 import "@usecapsule/react-sdk/styles.css";
 import Logo from "./assets/capsule-logo.svg";
+import { capsuleClient } from "./client/capsule";
 
 export default function App() {
   const [isModalOpen, setIsModalOpen] = useState(false);
 
   return (
     <div style={{ textAlign: "center", marginTop: "50px" }}>
-      <h1>Capsule Modal Starter Template with React and Vite</h1>
+      <h1>Capsule Modal Starter Template with React + Vite</h1>
       <button
         onClick={() => setIsModalOpen(true)}
         style={{ padding: "10px 20px", fontSize: "16px", cursor: "pointer" }}>
