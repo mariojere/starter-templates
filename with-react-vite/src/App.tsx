09ac614d5bab774e7e0f448767400496992e7a55
Jordy McNab <jordy@usecapsule.com>
2025-01-07 21:14:36 -0600

    chore: remove rn and version bump

--- with-react-vite/src/App.tsx
+++ with-react-vite/src/App.tsx
@@ -1,56 +1,35 @@
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { AuthLayout, CapsuleModal, ExternalWallet, OAuthMethod } from "@usecapsule/react-sdk";
-import {
-  CapsuleEvmProvider,
-  metaMaskWallet,
-  rainbowWallet,
-  walletConnectWallet,
-} from "@usecapsule/evm-wallet-connectors";
 import "@usecapsule/react-sdk/styles.css";
 import { useState } from "react";
-import { sepolia } from "wagmi/chains";
 
 import { capsuleClient } from "./client/capsule";
 import Logo from "./assets/capsule-logo.svg";
-
-const queryClient = new QueryClient();
 
 export default function Home() {
   const [isModalOpen, setIsModalOpen] = useState(false);
 
   return (
-    <QueryClientProvider client={queryClient}>
-      <CapsuleEvmProvider
-        config={{
-          appName: "Capsule NextJS Starter Template",
-          chains: [sepolia],
-          projectId: import.meta.env.VITE_WALLET_CONNECT_PROJECT_ID || "",
-          ssr: false,
-          wallets: [metaMaskWallet, walletConnectWallet, rainbowWallet],
-        }}>
-        <div style={{ textAlign: "center", marginTop: "50px" }}>
-          <h1>Capsule Modal Starter Template with Next.js</h1>
-          <button
-            onClick={() => setIsModalOpen(true)}
-            style={{ padding: "10px 20px", fontSize: "16px", cursor: "pointer" }}>
-            Open Capsule Modal
-          </button>
-          <CapsuleModal
-            appName="Capsule Modal Starter Template"
-            authLayout={[AuthLayout.AUTH_FULL, AuthLayout.EXTERNAL_CONDENSED]}
-            capsule={capsuleClient}
-            disableEmailLogin={false}
-            disablePhoneLogin={false}
-            externalWallets={[ExternalWallet.METAMASK, ExternalWallet.WALLETCONNECT, ExternalWallet.RAINBOW]}
-            isOpen={isModalOpen}
-            logo={Logo}
-            oAuthMethods={Object.values(OAuthMethod)}
-            onClose={() => setIsModalOpen(false)}
-            onRampTestMode={true}
-            twoFactorAuthEnabled={false}
-          />
-        </div>
-      </CapsuleEvmProvider>
-    </QueryClientProvider>
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>Capsule Modal Starter Template with Next.js</h1>
+      <button
+        onClick={() => setIsModalOpen(true)}
+        style={{ padding: "10px 20px", fontSize: "16px", cursor: "pointer" }}>
+        Open Capsule Modal
+      </button>
+      <CapsuleModal
+        appName="Capsule Modal Starter Template"
+        authLayout={[AuthLayout.AUTH_FULL, AuthLayout.EXTERNAL_CONDENSED]}
+        capsule={capsuleClient}
+        disableEmailLogin={false}
+        disablePhoneLogin={false}
+        externalWallets={[ExternalWallet.METAMASK, ExternalWallet.WALLETCONNECT, ExternalWallet.RAINBOW]}
+        isOpen={isModalOpen}
+        logo={Logo}
+        oAuthMethods={Object.values(OAuthMethod)}
+        onClose={() => setIsModalOpen(false)}
+        onRampTestMode={true}
+        twoFactorAuthEnabled={false}
+      />
+    </div>
   );
 }
